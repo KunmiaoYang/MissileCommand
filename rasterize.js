c06a30fdea813aee1308247041f2da1602c456e6
kyang12 <kyang12@ncsu.edu>
2017-11-25 20:21:55 -0500

    encapsulate shader

--- rasterize.js
+++ rasterize.js
@@ -10,196 +10,17 @@ const DELTA_TRANS = 0.0125; const DELTA_ROT = 0.02;
 var Eye = new vec4.fromValues(0.5,0.5,-0.5,1.0); // default eye position in world space
 var LookAt = vec3.fromValues(0, 0, 1); // default eye look at direction in world space
 var ViewUp = vec3.fromValues(0, 1, 0); // default eye view up direction in world space
-
-/* webgl globals */
-var gl = null; // the all powerful gl object. It's all here folks!
-var shaderProgram;
-var vertexPositionAttrib; // where to put position for vertex shader
-var vertexNormalAttrib; // where to put normal for vertex shader
-var uniforms = {};
-//endregion
-
-// ASSIGNMENT HELPER FUNCTIONS
-
-//region Set up environment
-// Set up the webGL environment
-function setupWebGL() {
-
-    // Get the canvas and context
-    var canvas = document.getElementById("myWebGLCanvas"); // create a js canvas
-    gl = canvas.getContext("webgl"); // get a webgl object from it
-    gl.viewportWidth = canvas.width; // store width
-    gl.viewportHeight = canvas.height; // store height
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    try {
-      if (gl == null) {
-        throw "unable to create gl context -- is your browser gl ready?";
-      } else {
-        gl.clearColor(0.0, 0.0, 0.0, 1.0); // use black when we clear the frame buffer
-        gl.clearDepth(1.0); // use max when we clear the depth buffer
-        gl.enable(gl.DEPTH_TEST); // use hidden surface removal (with zbuffering)
-      }
-    } // end try
-    
-    catch(e) {
-      console.log(e);
-    } // end catch
- 
-} // end setupWebGL
-
-// Set up the webGL shaders
-function setupShaders() {
-
-    // define fragment shader in essl using es6 template strings
-    var fShaderCode = `
-        
-        precision mediump float;
-        struct light_struct {
-          vec3 xyz;
-          vec3 ambient;
-          vec3 diffuse;
-          vec3 specular;
-        };
-        struct material_struct {
-          vec3 ambient;
-          vec3 diffuse;
-          vec3 specular;
-          float n;
-        };
-        
-        uniform light_struct uLights[N_LIGHT];
-        uniform material_struct uMaterial;
-        uniform int uLightModel;
-        
-        varying vec3 vTransformedNormal;
-        varying vec4 vPosition;
-        varying vec3 vCameraDirection;
-
-        void main(void) {
-            vec3 rgb = vec3(0, 0, 0);
-            
-            if(uLightModel < 0) {
-                rgb = uMaterial.diffuse;
-            } else {
-                for(int i = 0; i < N_LIGHT; i++) {
-                    vec3 L = normalize(uLights[i].xyz - vPosition.xyz);
-                    vec3 V = normalize(vCameraDirection);
-                    vec3 N = normalize(vTransformedNormal);
-                    float dVN = dot(V, N);
-                    float dLN = dot(L, N);
-                    rgb += uMaterial.ambient * uLights[i].ambient; // Ambient shading
-                    if(dLN > 0.0 && dVN > 0.0) {
-                        rgb += dLN * (uMaterial.diffuse * uLights[i].diffuse);      // Diffuse shading
-                        if(0 == uLightModel) {          // Phong specular shading
-                            vec3 R = normalize(2.0 * dot(N, L) * N - L);
-                            float weight = pow(dot(V, R), uMaterial.n);
-                            if(weight > 0.0) rgb += weight * (uMaterial.specular * uLights[i].specular);
-                        } else if(1 == uLightModel) {          // Blinn-Phong specular shading
-                            vec3 H = normalize(V + L);
-                            float weight = pow(dot(N, H), uMaterial.n);
-                            if(weight > 0.0) rgb += weight * (uMaterial.specular * uLights[i].specular);
-                        }
-                    }
-                }
-            }
-            gl_FragColor = vec4(rgb, 1); // all fragments are white
-        }
-    `;
-    fShaderCode = "#define N_LIGHT " + LIGHTS.array.length + "\n" + fShaderCode;
-
-    // define vertex shader in essl using es6 template strings
-    var vShaderCode = `
-        attribute vec3 vertexPosition;
-        attribute vec3 vertexNormal;
-
-        uniform mat4 uMMatrix;      // Model transformation
-        uniform mat4 uVMatrix;      // Viewing transformation
-        uniform mat4 uPMatrix;      // Projection transformation
-        uniform mat3 uNMatrix;      // Normal vector transformation
-        uniform vec3 uCameraPos;    // Camera position
-        uniform bool uDoubleSide;
-        
-        varying vec3 vTransformedNormal;
-        varying vec4 vPosition;
-        varying vec3 vCameraDirection;
-
-        void main(void) {
-            vPosition = uMMatrix * vec4(vertexPosition, 1.0);
-            vCameraDirection = uCameraPos - vPosition.xyz;
-            gl_Position = uPMatrix * uVMatrix * vPosition;
-            vTransformedNormal = uNMatrix * vertexNormal;
-            if(uDoubleSide && dot(vCameraDirection, vTransformedNormal) < 0.0)
-                vTransformedNormal = -vTransformedNormal;
-        }
-    `;
-
-    try {
-        // console.log("fragment shader: "+fShaderCode);
-        var fShader = gl.createShader(gl.FRAGMENT_SHADER); // create frag shader
-        gl.shaderSource(fShader,fShaderCode); // attach code to shader
-        gl.compileShader(fShader); // compile the code for gpu execution
-
-        // console.log("vertex shader: "+vShaderCode);
-        var vShader = gl.createShader(gl.VERTEX_SHADER); // create vertex shader
-        gl.shaderSource(vShader,vShaderCode); // attach code to shader
-        gl.compileShader(vShader); // compile the code for gpu execution
-
-        if (!gl.getShaderParameter(fShader, gl.COMPILE_STATUS)) { // bad frag shader compile
-            throw "error during fragment shader compile: " + gl.getShaderInfoLog(fShader);
-            gl.deleteShader(fShader);
-        } else if (!gl.getShaderParameter(vShader, gl.COMPILE_STATUS)) { // bad vertex shader compile
-            throw "error during vertex shader compile: " + gl.getShaderInfoLog(vShader);
-            gl.deleteShader(vShader);
-        } else { // no compile errors
-            shaderProgram = gl.createProgram(); // create the single shader program
-            gl.attachShader(shaderProgram, fShader); // put frag shader in program
-            gl.attachShader(shaderProgram, vShader); // put vertex shader in program
-            gl.linkProgram(shaderProgram); // link program into gl context
-
-            if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) { // bad program link
-                throw "error during shader program linking: " + gl.getProgramInfoLog(shaderProgram);
-            } else { // no shader program link errors
-                gl.useProgram(shaderProgram); // activate shader program (frag and vert)
-                vertexPositionAttrib = // get pointer to vertex shader input
-                    gl.getAttribLocation(shaderProgram, "vertexPosition");
-                gl.enableVertexAttribArray(vertexPositionAttrib); // input to shader from array
-
-                vertexNormalAttrib = gl.getAttribLocation(shaderProgram, "vertexNormal");
-                gl.enableVertexAttribArray(vertexNormalAttrib); // input to shader from array
-
-                // Get uniform matrices
-                uniforms.lightModelUniform = gl.getUniformLocation(shaderProgram, "uLightModel");
-                uniforms.cameraPosUniform = gl.getUniformLocation(shaderProgram, "uCameraPos");
-                uniforms.mMatrixUniform = gl.getUniformLocation(shaderProgram, "uMMatrix");
-                uniforms.vMatrixUniform = gl.getUniformLocation(shaderProgram, "uVMatrix");
-                uniforms.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-                uniforms.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
-                uniforms.doubleSideUniform = gl.getUniformLocation(shaderProgram, "uDoubleSide");
-                uniforms.materialUniform = MODELS.getMaterialUniformLocation(gl, shaderProgram, "uMaterial");
-                uniforms.lightUniformArray = [];
-                for (let i = 0; i < LIGHTS.array.length; i++) {
-                    uniforms.lightUniformArray[i] = LIGHTS.getLightUniformLocation(gl, shaderProgram, "uLights[" + i + "]");
-                }
-            } // end if no shader program link errors
-        } // end if no compile errors
-    } // end try
-
-    catch(e) {
-        console.log(e);
-    } // end catch
-} // end setup shaders
 //endregion
 
 // render the loaded model
 function renderTriangles() {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // clear frame/depth buffers
+    SHADER.gl.clear(SHADER.gl.COLOR_BUFFER_BIT | SHADER.gl.DEPTH_BUFFER_BIT); // clear frame/depth buffers
 
-    gl.uniform3fv(uniforms.cameraPosUniform, CAMERA.xyz);
-    gl.uniformMatrix4fv(uniforms.vMatrixUniform, false, CAMERA.vMatrix);
-    gl.uniformMatrix4fv(uniforms.pMatrixUniform, false, CAMERA.pMatrix);
+    SHADER.gl.uniform3fv(SHADER.uniforms.cameraPosUniform, CAMERA.xyz);
+    SHADER.gl.uniformMatrix4fv(SHADER.uniforms.vMatrixUniform, false, CAMERA.vMatrix);
+    SHADER.gl.uniformMatrix4fv(SHADER.uniforms.pMatrixUniform, false, CAMERA.pMatrix);
     for (let i = 0; i < LIGHTS.array.length; i++) {
-        LIGHTS.setLightUniform(gl, uniforms.lightUniformArray[i], LIGHTS.array[i]);
+        LIGHTS.setLightUniform(SHADER.gl, SHADER.uniforms.lightUniformArray[i], LIGHTS.array[i]);
     }
 
     var scaleMatrix = mat4.identity(mat4.create());
@@ -207,30 +28,30 @@ function renderTriangles() {
 
     for(let i = 0; i < MODELS.array.length; i++) {
         if(OPTION.useLight)
-            gl.uniform1i(uniforms.lightModelUniform, MODELS.array[i].specularModel);
+            SHADER.gl.uniform1i(SHADER.uniforms.lightModelUniform, MODELS.array[i].specularModel);
         else
-            gl.uniform1i(uniforms.lightModelUniform, -1);
+            SHADER.gl.uniform1i(SHADER.uniforms.lightModelUniform, -1);
         // triangleSetArray[i].material.ambient = [0.5,1.0,1.0];
-        gl.uniform1f(uniforms.doubleSideUniform, MODELS.array[i].doubleSide);
-        MODELS.setMaterialUniform(gl, uniforms.materialUniform, MODELS.array[i].material);
+        SHADER.gl.uniform1f(SHADER.uniforms.doubleSideUniform, MODELS.array[i].doubleSide);
+        MODELS.setMaterialUniform(SHADER.gl, SHADER.uniforms.materialUniform, MODELS.array[i].material);
         var mMatrix = mat4.multiply(mat4.create(), MODELS.array[i].tMatrix, MODELS.array[i].rMatrix);
         if (MODELS.selectId === i) {
             mMatrix = mat4.multiply(mat4.create(), mMatrix, scaleMatrix);
         }
-        gl.uniformMatrix4fv(uniforms.mMatrixUniform, false, mMatrix);
-        gl.uniformMatrix3fv(uniforms.nMatrixUniform, false, mat3.normalFromMat4(mat3.create(), MODELS.array[i].rMatrix));
+        SHADER.gl.uniformMatrix4fv(SHADER.uniforms.mMatrixUniform, false, mMatrix);
+        SHADER.gl.uniformMatrix3fv(SHADER.uniforms.nMatrixUniform, false, mat3.normalFromMat4(mat3.create(), MODELS.array[i].rMatrix));
 
         // vertex buffer: activate and feed into vertex shader
-        gl.bindBuffer(gl.ARRAY_BUFFER, MODELS.array[i].vertexBuffer); // activate
-        gl.vertexAttribPointer(vertexPositionAttrib,3,gl.FLOAT,false,0,0); // feed
+        SHADER.gl.bindBuffer(SHADER.gl.ARRAY_BUFFER, MODELS.array[i].vertexBuffer); // activate
+        SHADER.gl.vertexAttribPointer(SHADER.vertexPositionAttrib,3,SHADER.gl.FLOAT,false,0,0); // feed
 
         // vertex normal buffer: activate and feed into vertex shader
-        gl.bindBuffer(gl.ARRAY_BUFFER, MODELS.array[i].normalBuffer); // activate
-        gl.vertexAttribPointer(vertexNormalAttrib,3,gl.FLOAT,false,0,0); // feed
+        SHADER.gl.bindBuffer(SHADER.gl.ARRAY_BUFFER, MODELS.array[i].normalBuffer); // activate
+        SHADER.gl.vertexAttribPointer(SHADER.vertexNormalAttrib,3,SHADER.gl.FLOAT,false,0,0); // feed
 
         // triangle buffer: activate and render
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, MODELS.array[i].triangleBuffer); // activate
-        gl.drawElements(gl.TRIANGLES, MODELS.array[i].triBufferSize,gl.UNSIGNED_SHORT,0); // render
+        SHADER.gl.bindBuffer(SHADER.gl.ELEMENT_ARRAY_BUFFER, MODELS.array[i].triangleBuffer); // activate
+        SHADER.gl.drawElements(SHADER.gl.TRIANGLES, MODELS.array[i].triBufferSize,SHADER.gl.UNSIGNED_SHORT,0); // render
     }
 } // end render triangles
 
@@ -240,16 +61,17 @@ function setupOnLoad() {
         if (!LIGHTS.ready) {
             console.log('LIGHTS not ready!');
         } else {
-            setupShaders(); // setup the webGL shaders
+            SHADER.setupShaders(); // setup the webGL shaders
             renderTriangles(); // draw the triangles using webGL
         }
     });
 }
 
+// refresh image when settings are changed
 function refresh() {
     DOM.load(OPTION, CAMERA, URL);   // load the data from html page
     LIGHTS.load(); // load in the lights
-    setupWebGL(); // set up the webGL environment
+    SHADER.setupWebGL(); // set up the webGL environment
     CAMERA.pMatrix = CAMERA.calcPerspective(CAMERA.left, CAMERA.right, CAMERA.top, CAMERA.bottom, CAMERA.near, CAMERA.far);
 }
 
@@ -257,10 +79,10 @@ function refresh() {
 function main() {
     DOM.load(OPTION, CAMERA, URL);   // load the data from html page
     LIGHTS.load(); // load in the lights
-    setupWebGL(); // set up the webGL environment
+    SHADER.setupWebGL(); // set up the webGL environment
     CAMERA.initCamera(Eye, LookAt, ViewUp); // Initialize camera
-    JSON_MODEL.loadTriangleSets(gl); // load in the triangles from tri file
-    JSON_MODEL.loadEllipsoids(gl); // load in the ellipsoids from ellipsoids file
+    JSON_MODEL.loadTriangleSets(SHADER.gl); // load in the triangles from tri file
+    JSON_MODEL.loadEllipsoids(SHADER.gl); // load in the ellipsoids from ellipsoids file
     EVENTS.setupKeyEvent();
     setupOnLoad();
 } // end main
---
+++ shader.js
@@ -0,0 +1,176 @@
+var SHADER = function() {
+    // define fragment shader in essl using es6 template strings
+    function getFShaderCode(nLight) {
+        return "#define N_LIGHT " + nLight + "\n" +
+            `
+            
+        precision mediump float;
+        struct light_struct {
+          vec3 xyz;
+          vec3 ambient;
+          vec3 diffuse;
+          vec3 specular;
+        };
+        struct material_struct {
+          vec3 ambient;
+          vec3 diffuse;
+          vec3 specular;
+          float n;
+        };
+        
+        uniform light_struct uLights[N_LIGHT];
+        uniform material_struct uMaterial;
+        uniform int uLightModel;
+        
+        varying vec3 vTransformedNormal;
+        varying vec4 vPosition;
+        varying vec3 vCameraDirection;
+
+        void main(void) {
+            vec3 rgb = vec3(0, 0, 0);
+            
+            if(uLightModel < 0) {
+                rgb = uMaterial.diffuse;
+            } else {
+                for(int i = 0; i < N_LIGHT; i++) {
+                    vec3 L = normalize(uLights[i].xyz - vPosition.xyz);
+                    vec3 V = normalize(vCameraDirection);
+                    vec3 N = normalize(vTransformedNormal);
+                    float dVN = dot(V, N);
+                    float dLN = dot(L, N);
+                    rgb += uMaterial.ambient * uLights[i].ambient; // Ambient shading
+                    if(dLN > 0.0 && dVN > 0.0) {
+                        rgb += dLN * (uMaterial.diffuse * uLights[i].diffuse);      // Diffuse shading
+                        if(0 == uLightModel) {          // Phong specular shading
+                            vec3 R = normalize(2.0 * dot(N, L) * N - L);
+                            float weight = pow(dot(V, R), uMaterial.n);
+                            if(weight > 0.0) rgb += weight * (uMaterial.specular * uLights[i].specular);
+                        } else if(1 == uLightModel) {          // Blinn-Phong specular shading
+                            vec3 H = normalize(V + L);
+                            float weight = pow(dot(N, H), uMaterial.n);
+                            if(weight > 0.0) rgb += weight * (uMaterial.specular * uLights[i].specular);
+                        }
+                    }
+                }
+            }
+            gl_FragColor = vec4(rgb, 1); // all fragments are white
+        }
+        `;
+    }
+    // define vertex shader in essl using es6 template strings
+    function getVShaderCode() {
+        return `
+        attribute vec3 vertexPosition;
+        attribute vec3 vertexNormal;
+
+        uniform mat4 uMMatrix;      // Model transformation
+        uniform mat4 uVMatrix;      // Viewing transformation
+        uniform mat4 uPMatrix;      // Projection transformation
+        uniform mat3 uNMatrix;      // Normal vector transformation
+        uniform vec3 uCameraPos;    // Camera position
+        uniform bool uDoubleSide;
+        
+        varying vec3 vTransformedNormal;
+        varying vec4 vPosition;
+        varying vec3 vCameraDirection;
+
+        void main(void) {
+            vPosition = uMMatrix * vec4(vertexPosition, 1.0);
+            vCameraDirection = uCameraPos - vPosition.xyz;
+            gl_Position = uPMatrix * uVMatrix * vPosition;
+            vTransformedNormal = uNMatrix * vertexNormal;
+            if(uDoubleSide && dot(vCameraDirection, vTransformedNormal) < 0.0)
+                vTransformedNormal = -vTransformedNormal;
+        }
+        `;
+    }
+    return {
+        gl: null,
+        shaderProgram: null,
+        vertexPositionAttrib: null,
+        vertexNormalAttrib: null,
+        uniforms: {},
+        setupWebGL: function() {
+
+            // Get the canvas and context
+            var canvas = document.getElementById("myWebGLCanvas"); // create a js canvas
+            SHADER.gl = canvas.getContext("webgl"); // get a webgl object from it
+            SHADER.gl.viewportWidth = canvas.width; // store width
+            SHADER.gl.viewportHeight = canvas.height; // store height
+            SHADER.gl.viewport(0, 0, SHADER.gl.canvas.width, SHADER.gl.canvas.height);
+
+            try {
+                if (SHADER.gl == null) {
+                    throw "unable to create gl context -- is your browser gl ready?";
+                } else {
+                    SHADER.gl.clearColor(0.0, 0.0, 0.0, 1.0); // use black when we clear the frame buffer
+                    SHADER.gl.clearDepth(1.0); // use max when we clear the depth buffer
+                    SHADER.gl.enable(SHADER.gl.DEPTH_TEST); // use hidden surface removal (with zbuffering)
+                }
+            } // end try
+
+            catch(e) {
+                console.log(e);
+            } // end catch
+
+        },
+        setupShaders: function() {
+        let fShaderCode = getFShaderCode(LIGHTS.array.length);
+        let vShaderCode = getVShaderCode();
+        try {
+            // console.log("fragment shader: "+fShaderCode);
+            var fShader = SHADER.gl.createShader(SHADER.gl.FRAGMENT_SHADER); // create frag shader
+            SHADER.gl.shaderSource(fShader,fShaderCode); // attach code to shader
+            SHADER.gl.compileShader(fShader); // compile the code for gpu execution
+
+            // console.log("vertex shader: "+vShaderCode);
+            var vShader = SHADER.gl.createShader(SHADER.gl.VERTEX_SHADER); // create vertex shader
+            SHADER.gl.shaderSource(vShader,vShaderCode); // attach code to shader
+            SHADER.gl.compileShader(vShader); // compile the code for gpu execution
+
+            if (!SHADER.gl.getShaderParameter(fShader, SHADER.gl.COMPILE_STATUS)) { // bad frag shader compile
+                throw "error during fragment shader compile: " + SHADER.gl.getShaderInfoLog(fShader);
+                SHADER.gl.deleteShader(fShader);
+            } else if (!SHADER.gl.getShaderParameter(vShader, SHADER.gl.COMPILE_STATUS)) { // bad vertex shader compile
+                throw "error during vertex shader compile: " + SHADER.gl.getShaderInfoLog(vShader);
+                SHADER.gl.deleteShader(vShader);
+            } else { // no compile errors
+                SHADER.shaderProgram = SHADER.gl.createProgram(); // create the single shader program
+                SHADER.gl.attachShader(SHADER.shaderProgram, fShader); // put frag shader in program
+                SHADER.gl.attachShader(SHADER.shaderProgram, vShader); // put vertex shader in program
+                SHADER.gl.linkProgram(SHADER.shaderProgram); // link program into gl context
+
+                if (!SHADER.gl.getProgramParameter(SHADER.shaderProgram, SHADER.gl.LINK_STATUS)) { // bad program link
+                    throw "error during shader program linking: " + SHADER.gl.getProgramInfoLog(SHADER.shaderProgram);
+                } else { // no shader program link errors
+                    SHADER.gl.useProgram(SHADER.shaderProgram); // activate shader program (frag and vert)
+                    SHADER.vertexPositionAttrib = // get pointer to vertex shader input
+                        SHADER.gl.getAttribLocation(SHADER.shaderProgram, "vertexPosition");
+                    SHADER.gl.enableVertexAttribArray(SHADER.vertexPositionAttrib); // input to shader from array
+
+                    SHADER.vertexNormalAttrib = SHADER.gl.getAttribLocation(SHADER.shaderProgram, "vertexNormal");
+                    SHADER.gl.enableVertexAttribArray(SHADER.vertexNormalAttrib); // input to shader from array
+
+                    // Get uniform matrices
+                    SHADER.uniforms.lightModelUniform = SHADER.gl.getUniformLocation(SHADER.shaderProgram, "uLightModel");
+                    SHADER.uniforms.cameraPosUniform = SHADER.gl.getUniformLocation(SHADER.shaderProgram, "uCameraPos");
+                    SHADER.uniforms.mMatrixUniform = SHADER.gl.getUniformLocation(SHADER.shaderProgram, "uMMatrix");
+                    SHADER.uniforms.vMatrixUniform = SHADER.gl.getUniformLocation(SHADER.shaderProgram, "uVMatrix");
+                    SHADER.uniforms.pMatrixUniform = SHADER.gl.getUniformLocation(SHADER.shaderProgram, "uPMatrix");
+                    SHADER.uniforms.nMatrixUniform = SHADER.gl.getUniformLocation(SHADER.shaderProgram, "uNMatrix");
+                    SHADER.uniforms.doubleSideUniform = SHADER.gl.getUniformLocation(SHADER.shaderProgram, "uDoubleSide");
+                    SHADER.uniforms.materialUniform = MODELS.getMaterialUniformLocation(SHADER.gl, SHADER.shaderProgram, "uMaterial");
+                    SHADER.uniforms.lightUniformArray = [];
+                    for (let i = 0; i < LIGHTS.array.length; i++) {
+                        SHADER.uniforms.lightUniformArray[i] = LIGHTS.getLightUniformLocation(SHADER.gl, SHADER.shaderProgram, "uLights[" + i + "]");
+                    }
+                } // end if no shader program link errors
+            } // end if no compile errors
+        } // end try
+
+        catch(e) {
+            console.log(e);
+        } // end catch
+    }
+    }
+}();
