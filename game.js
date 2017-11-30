83b3ac0cce5b7fa97f707ab4213b517316b62a9d
kyang12 <kyang12@ncsu.edu>
2017-11-30 10:21:38 -0500

    add defense missile

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var GAME = function() {
         pos: [0.875, 0.75, 0.625, 0.375, 0.25, 0.125],
         tMatrixArray: [],
         rMatrixArray: []
-    }
+    };
     for(let i = 0; i < CITY_COUNT; i++) {
         let rMatrix = mat4.create();
         city.rMatrixArray.push(mat4.scale(rMatrix, rMatrix, [CITY_SCALE, CITY_SCALE, CITY_SCALE]));
@@ -16,14 +16,39 @@ var GAME = function() {
         pos: [1, 0.5, 0],
         tMatrixArray: [],
         rMatrixArray: []
-    }
-    for(let i = 0; i < BATTERY_COUNT; i++) {
-        let rMatrix = mat4.create();
-        battery.rMatrixArray.push(mat4.scale(rMatrix, rMatrix, [BATTERY_SCALE, BATTERY_SCALE, BATTERY_SCALE]));
+    };
+    var defenseMissile = {
+        x: [-0.015, 0, 0.015],
+        z: [-0.015, 0, 0.015],
+        tMatrixArray: [],
+        rMatrixArray: []
+    };
+    for(let i = 0, idMatrix = mat4.create(); i < BATTERY_COUNT; i++) {
+        battery.rMatrixArray.push(mat4.scale(mat4.create(), idMatrix, [BATTERY_SCALE, BATTERY_SCALE, BATTERY_SCALE]));
         battery.tMatrixArray.push(mat4.fromTranslation(mat4.create(), [battery.pos[i], 0, 0]));
+        defenseMissile.rMatrixArray[i] = [];
+        defenseMissile.tMatrixArray[i] = [];
+        for(let j = 0; j < defenseMissile.x.length; j++) {
+            for(let k = 0; k < defenseMissile.z.length; k++) {
+                defenseMissile.rMatrixArray[i].push(mat4.scale(mat4.create(), idMatrix, [MISSILE_SCALE, MISSILE_SCALE, MISSILE_SCALE]));
+                defenseMissile.tMatrixArray[i].push(mat4.fromTranslation(mat4.create(), [battery.pos[i] + defenseMissile.x[j], 0, defenseMissile.z[k]]));
+            }
+        }
     }
     return {
         model: {},
+        initDefenseMissile: function () {
+            let missiles = [];
+            for(let i = 0; i < BATTERY_COUNT; i++) {
+                missiles[i] = [];
+                for(let j = 0, len = defenseMissile.tMatrixArray[i].length; j < len; j++) {
+                    missiles[i][j] = MODELS.shallowClone(GAME.model.missile.prototype);
+                    missiles[i][j].rMatrix = defenseMissile.rMatrixArray[i][j];
+                    missiles[i][j].tMatrix = defenseMissile.tMatrixArray[i][j];
+                }
+            }
+            GAME.model.defenseMissiles = missiles;
+        },
         init: function() {
             GAME.model.background = JSON_MODEL.loadTriangleSets(SHADER.gl);
             SKECHUP_MODEL.loadModel(SHADER.gl, URL.cityModel, function (model) {
@@ -44,15 +69,15 @@ var GAME = function() {
             });
             SKECHUP_MODEL.loadModel(SHADER.gl, URL.missileModel, function (model) {
                 GAME.model.missile = {prototype: model};
-                let rMatrix = mat4.create();
-                GAME.model.missile.prototype.rMatrix = mat4.scale(rMatrix, rMatrix, [MISSILE_SCALE, MISSILE_SCALE, MISSILE_SCALE]);
-                // GAME.model.missile.prototype.tMatrix[13] = 0.1;
+                GAME.initDefenseMissile();
             })
         },
         play: function () {
             MODELS.array = GAME.model.background.concat(GAME.model.cities);
             MODELS.array = MODELS.array.concat(GAME.model.batteries);
-            MODELS.array.push(GAME.model.missile.prototype);
+            for (let i = 0; i < BATTERY_COUNT; i++) {
+                MODELS.array = MODELS.array.concat(GAME.model.defenseMissiles[i]);
+            }
             renderTriangles();
         }
     }
