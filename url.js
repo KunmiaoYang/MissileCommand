2f98d0f986bf9bf99a3f9c97ed4e6cda13fbbb7b
kyang12 <kyang12@ncsu.edu>
2017-12-02 17:36:20 -0500

    adjust mountain model

--- url.js
+++ url.js
@@ -1,11 +1,11 @@
 var URL = function () {
     return {
-        triangles: 'https://kunmiaoyang.github.io/Program4/triangles.json',
-        ellipsoids: 'ellipsoids.json',
-        lights: 'https://kunmiaoyang.github.io/Program4/lights.json',
+        triangles: 'https://kunmiaoyang.github.io/Program4/model/triangles.json',
+        ellipsoids: 'model/ellipsoids.json',
+        lights: 'model/lights.json',
         cityModel: 'https://kunmiaoyang.github.io/Program4/model/city.dae',
         batteryModel: 'https://kunmiaoyang.github.io/Program4/model/battery.dae',
-        mountainModel: 'https://kunmiaoyang.github.io/Program4/model/mountain.dae',
+        mountainModel: 'model/mountain.dae',
         missileModel: 'https://kunmiaoyang.github.io/Program4/model/missile.dae',
         UFOModel: 'https://kunmiaoyang.github.io/Program4/model/UFO.dae'
     };
