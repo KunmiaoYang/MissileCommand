15db1eb806db460ca8aab429e20dec1d1b0612e8
kyang12 <kyang12@ncsu.edu>
2017-12-02 17:49:33 -0500

    adjust light

--- url.js
+++ url.js
@@ -2,10 +2,10 @@ var URL = function () {
     return {
         triangles: 'https://kunmiaoyang.github.io/Program4/model/triangles.json',
         ellipsoids: 'model/ellipsoids.json',
-        lights: 'model/lights.json',
+        lights: 'https://kunmiaoyang.github.io/Program4/model/lights.json',
         cityModel: 'https://kunmiaoyang.github.io/Program4/model/city.dae',
         batteryModel: 'https://kunmiaoyang.github.io/Program4/model/battery.dae',
-        mountainModel: 'model/mountain.dae',
+        mountainModel: 'https://kunmiaoyang.github.io/Program4/model/mountain.dae',
         missileModel: 'https://kunmiaoyang.github.io/Program4/model/missile.dae',
         UFOModel: 'https://kunmiaoyang.github.io/Program4/model/UFO.dae'
     };
